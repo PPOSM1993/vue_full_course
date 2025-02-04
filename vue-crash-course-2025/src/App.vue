9460dcf26868e07c0b1dc686add1685888ce164a
PPOSM1993 <pedro.osorio04@inacapmail.cl>
2025-02-04 17:45:49 -0300

    avance curso

---
+++ vue-crash-course-2025/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Pedro Osorio',
+      status: false,
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{name}}</h1>
+  <br>
+  <p v-if="status">User is activate</p>
+  <p v-else="status">User is inactivate</p>
+</template>
+
+<style>
+
+* {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+}
+
+</style>
